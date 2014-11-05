7a54e5ac56d0bf9970a7ed76441fde2947c83195
Felix Hamel <felixhamel@me.com>
2014-11-05 14:40:14 -0500

    Ajout de la classe des Arbitres

--- src/ligueBaseball/entities/Official.java
+++ src/ligueBaseball/entities/Official.java
@@ -1,6 +1,128 @@
 package ligueBaseball.entities;
 
-public class Official
-{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import ligueBaseball.exceptions.FailedToDeleteEntityException;
+import ligueBaseball.exceptions.FailedToRetrieveNextKeyFromSequenceException;
+import ligueBaseball.exceptions.FailedToSaveEntityException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Official extends DatabaseEntity
+{
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Get the official with the given ID.
+     *
+     * @param databaseConnection
+     * @param id - ID of the official.
+     * @return Officiel - If found, otherwise return null.
+     */
+    public static Official getOfficialWithId(Connection databaseConnection, int id)
+    {
+        PreparedStatement statement = null;
+
+        try {
+            statement = databaseConnection.prepareStatement("SELECT * FROM arbitre WHERE arbitreid = ?;");
+            statement.setInt(1, id);
+
+            ResultSet officialResult = statement.executeQuery();
+            if (!officialResult.next()) {
+                return null;
+            }
+            return getEntityFromResultSet(officialResult);
+
+        } catch (SQLException e) {
+            return null;
+
+        } finally {
+            closeStatement(statement);
+        }
+    }
+
+    protected static Official getEntityFromResultSet(ResultSet resultSet) throws SQLException
+    {
+        Official entity = new Official();
+        entity.id = resultSet.getInt("arbitreid");
+        entity.firstName = resultSet.getString("arbitreprenom");
+        entity.lastName = resultSet.getString("arbitrenom");
+
+        return entity;
+    }
+
+    @Override
+    protected void create(Connection databaseConnection) throws FailedToSaveEntityException
+    {
+        PreparedStatement statement = null;
+        try {
+            id = getNextIdForTable(databaseConnection, "arbitre", "arbitreid");
+            statement = databaseConnection.prepareStatement("INSERT INTO arbitre (arbitreid, arbitreprenom, arbitrenom) VALUES(?, ?' ?);");
+            statement.setInt(1, id);
+            statement.setString(2, firstName);
+            statement.setString(3, lastName);
+            statement.execute();
+            databaseConnection.commit();
+
+        } catch (SQLException | FailedToRetrieveNextKeyFromSequenceException e) {
+            throw new FailedToSaveEntityException(e);
+        } finally {
+            closeStatement(statement);
+        }
+    }
+
+    @Override
+    protected void update(Connection databaseConnection) throws FailedToSaveEntityException
+    {
+        PreparedStatement statement = null;
+        try {
+            statement = databaseConnection.prepareStatement("UPDATE arbitre SET arbitreprenom = ? AND arbitrenom = ? WHERE arbitreid = ?;");
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            statement.setInt(3, id);
+            statement.executeUpdate();
+            databaseConnection.commit();
+
+        } catch (SQLException e) {
+            throw new FailedToSaveEntityException(e);
+        } finally {
+            closeStatement(statement);
+        }
+    }
+
+    @Override
+    public void delete(Connection databaseConnection) throws FailedToDeleteEntityException, Exception
+    {
+        /*
+         * f (id >= 0) { PreparedStatement statement = null; try { statement = databaseConnection.prepareStatement("DELETE arbitre"); }
+         * 
+         * }
+         * 
+         * firstName = null; lastName = null;
+         */
+        throw new NotImplementedException(); // Not needed for the moment
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
 }
